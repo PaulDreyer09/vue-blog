8991504116db963ffa3ed6d75f46a2eb297f50ca
PaulDreyer09 <52189254+PaulDreyer09@users.noreply.github.com>
2023-08-27 17:11:28 +0200

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+
   </div>
 </template>
 
 <script>
+import { ref, computed, watch, watchEffect } from 'vue';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const posts = ref([
+      {title: 'Welcome to the blog', body: 'Lorem ipsum', id: 1},
+      {title: 'Top 5 CSS tips', body: 'Lorem ipsum', id: 2},
+
+    ])
+
+    return {
+
+    }
   }
 }
 </script>
